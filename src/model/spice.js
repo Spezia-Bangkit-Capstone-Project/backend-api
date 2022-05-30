c3170403c94dab2bbe5aee71239a4c29e75925a9
Ade Iqbal <adeiqbal1305@gmail.com>
2022-05-30 14:00:16 +0700

    create model spice and spice benefit

---
+++ src/model/spice.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require("sequelize");
+const dbConnection = require("../database/config");
+
+const spice = dbConnection.define(
+  "spice",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    latin_name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.BIGINT,
+    },
+    updated_at: {
+      type: DataTypes.BIGINT,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = spice;
